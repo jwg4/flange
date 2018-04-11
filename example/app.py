d52796746bdd496de85c57b7f26f0a8dcb641916
jwg4 <jwg4@users.noreply.github.com>
2018-04-11 15:20:33 +0000

    serve static files

--- example/app.py
+++ example/app.py
@@ -15,6 +15,9 @@ def get_chickens():
     }
     return data
 
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
 
 if __name__ == '__main__':
     app.run()
